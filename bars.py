82d860a85c8453ae5c08a91f550d32675965d974
ignat <buyfng@gmail.com>
2017-06-01 20:41:16 +0300

    complied to pep8

--- bars.py
+++ bars.py
@@ -10,35 +10,43 @@ def load_data(filepath):
     with open(filepath, 'r') as file_handler:
         return json.load(file_handler)
 
-    
+
 def print_bar(bar):
     print('Название: ' + bar['Name'])
     print('Количество мест: ' + str(bar['SeatsCount']))
-    print('Адрес: ' + bar['Address'])    
+    print('Адрес: ' + bar['Address'])
 
 
 def get_biggest_bar(data):
-    return reduce(lambda bar1, bar2: bar1 if (bar1['SeatsCount'] > bar2['SeatsCount']) else bar2,
+    return reduce(lambda bar1, bar2:
+                  bar1 if (bar1['SeatsCount'] > bar2['SeatsCount']) else bar2,
                   data)
 
 
 def get_smallest_bar(data):
-    return reduce(lambda bar1, bar2: bar1 if (bar1['SeatsCount'] < bar2['SeatsCount']) else bar2,
+    return reduce(lambda bar1, bar2:
+                  bar1 if (bar1['SeatsCount'] < bar2['SeatsCount']) else bar2,
                   data)
 
 
 def get_closest_bar(data, longitude, latitude):
     def closer_bar(bar1, bar2):
-        dist1 = distance_between_points(latitude, longitude, float(bar1['Latitude_WGS84']), float(bar1['Longitude_WGS84']))
-        dist2 = distance_between_points(latitude, longitude, float(bar2['Latitude_WGS84']), float(bar2['Longitude_WGS84']))
+        dist1 = distance_between_points(latitude,
+                                        longitude,
+                                        float(bar1['Latitude_WGS84']),
+                                        float(bar1['Longitude_WGS84']))
+        dist2 = distance_between_points(latitude,
+                                        longitude,
+                                        float(bar2['Latitude_WGS84']),
+                                        float(bar2['Longitude_WGS84']))
         return bar2 if dist2 < dist1 else bar1
 
     return reduce(closer_bar, data)
 
 
 def distance_between_points(latitude1, longitude1, latitude2, longitude2):
-    distY = latitude2 - latitude1;
-    distX = longitude2 - longitude1;
+    distY = latitude2 - latitude1
+    distX = longitude2 - longitude1
     return math.sqrt((distX**2 + distY**2))
 
 
